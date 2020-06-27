6d4e253e55608a927d5d7c2f12e1292be64a0099
zhangenming <282126346@qq.com>
2020-06-27 15:25:00 +0800

    重写 以前的代码逻辑好像都作废了白写了（写着写着就用其他逻辑代替了）  需要重新理下为什么

--- src/sdk.js
+++ src/sdk.js
@@ -1,49 +1,13 @@
-console.clear()
-
-function arrayDiff(A, B = A91) {
-  return B.filter(e => !A.includes(e))
-}
-function arrayIntersection(A, B, C) {
-  //todo 可变参数
-  return [...new Set([...A, ...B, ...C])].filter(
-    e => A.includes(e) && B.includes(e) && C.includes(e)
-  )
-}
-
-function cacheRunIt(f, iteration = A81) {
-  const cache = iteration.map(f)
-  return cache
-  return x => cache[x] //?
-}
-
-function deleteMaybe(arr, target) {
-  const index = arr.indexOf(target + "")
-  if (index === -1) return //必须返回, -1给splice会有其他效果
-  arr.splice(index, 1) //是否需要可变数组 git管理以防无法复原的bug
-  return arr
-}
-
-export function pos2index([x, y]) {
-  return y * 9 + x
-}
-
-function row2col(row) {
-  return A90.map(x => A90.map(y => row[y][x]))
-}
-function col2row(col) {
-  return A90.map(x => A90.map(y => col[y][x]))
-}
-
 export const NEEDRESOLVE = "0"
 export const A90 = [...Array(9)].map((_, i) => i)
 export const A91 = [...Array(9)].map((_, i) => i + 1)
+
 const A81 = [...Array(81)].map((_, i) => i)
 const A3 = [...Array(3)].map((_, i) => i)
-
-export const index2pos = cacheRunIt(i => [i % 9, Math.floor(i / 9)])
-
+const STR =
+  "010000050200000400040060900092654080060003000835920046000070060020800000400090570"
 //手动打孔 增加难度?
-const VALUE4ROW = (window.VALUE4ROW = [
+const VALUE_4Hang = (window.VALUE_4Hang = string2map() || [
   [2, 1, 6, 0, 8, 0, 9, 5, 7],
   [7, 8, 9, 0, 1, 0, 0, 0, 3],
   [5, 4, 0, 2, 9, 7, 0, 1, 6],
@@ -54,347 +18,163 @@ const VALUE4ROW = (window.VALUE4ROW = [
   [0, 9, 4, 0, 5, 0, 0, 0, 2],
   [3, 2, 5, 0, 7, 0, 4, 8, 1],
 ])
-// .map((e) => e.map((e) => "" + e)) //todo with ''
-const VALUE4COL = row2col(VALUE4ROW)
-const num2Hang9ItemOfIndex = A90.map(y => A90.map(x => y * 9 + x))
-const num2Liee9ItemOfIndex = row2col(num2Hang9ItemOfIndex)
 
-const VALUE81 = VALUE4ROW.flat()
-const index2Value = VALUE81.map(e => "" + e) //todo clear
+const VALUE_4Hang_ITEM = VALUE_4Hang.map(hang =>
+  hang
+    .map(e => "" + e)
+    .map(value => ({
+      value,
+      maybe: value === NEEDRESOLVE ? [...A91].map(e => "" + e) : [],
+    }))
+)
+window.s = VALUE_4Hang_ITEM
 
-//index -> index是第几个 块
-const index2NumOfArea = cacheRunIt(i => {
-  const [x, y] = index2pos[i]
-  const rowIndex = Math.floor(y / 3)
-  const colIndex = Math.floor(x / 3)
-  if (rowIndex === 0 && colIndex === 0) return 0
-  if (rowIndex === 0 && colIndex === 1) return 1
-  if (rowIndex === 0 && colIndex === 2) return 2
-  if (rowIndex === 1 && colIndex === 0) return 3
-  if (rowIndex === 1 && colIndex === 1) return 4
-  if (rowIndex === 1 && colIndex === 2) return 5
-  if (rowIndex === 2 && colIndex === 0) return 6
-  if (rowIndex === 2 && colIndex === 1) return 7
-  if (rowIndex === 2 && colIndex === 2) return 8
-})
-//index -> index是第几个 hang
-const index2NumOfHang = i => Math.floor(i / 9)
-//index -> index是第几个 lie
-const index2NumOfLiee = i => i % 9
-//上面两个集合
+export const index2pos = cacheRunIt(i => [i % 9, Math.floor(i / 9)], A81)
+
 export const index2flex = A3.map(y =>
   A3.map(x => A3.map(l => A3.map(r => [l + y * 3, r + x * 3])))
 )
   .flat(3)
   .map(e => "" + e)
-const _index2flex = [
-  [0, 0],
-  [0, 1],
-  [0, 2],
-  [1, 0],
-  [1, 1],
-  [1, 2],
-  [2, 0],
-  [2, 1],
-  [2, 2],
-  [0, 0 + 3],
-  [0, 1 + 3],
-  [0, 2 + 3],
-  [1, 0 + 3],
-  [1, 1 + 3],
-  [1, 2 + 3],
-  [2, 0 + 3],
-  [2, 1 + 3],
-  [2, 2 + 3],
-  [0, 0 + 3 * 2],
-  [0, 1 + 3 * 2],
-  [0, 2 + 3 * 2],
-  [1, 0 + 3 * 2],
-  [1, 1 + 3 * 2],
-  [1, 2 + 3 * 2],
-  [2, 0 + 3 * 2],
-  [2, 1 + 3 * 2],
-  [2, 2 + 3 * 2],
-  [0 + 3, 0],
-  [0 + 3, 1],
-  [0 + 3, 2],
-  [1 + 3, 0],
-  [1 + 3, 1],
-  [1 + 3, 2],
-  [2 + 3, 0],
-  [2 + 3, 1],
-  [2 + 3, 2],
-  [0 + 3, 0 + 3],
-  [0 + 3, 1 + 3],
-  [0 + 3, 2 + 3],
-  [1 + 3, 0 + 3],
-  [1 + 3, 1 + 3],
-  [1 + 3, 2 + 3],
-  [2 + 3, 0 + 3],
-  [2 + 3, 1 + 3],
-  [2 + 3, 2 + 3],
-  [0 + 3, 0 + 3 * 2],
-  [0 + 3, 1 + 3 * 2],
-  [0 + 3, 2 + 3 * 2],
-  [1 + 3, 0 + 3 * 2],
-  [1 + 3, 1 + 3 * 2],
-  [1 + 3, 2 + 3 * 2],
-  [2 + 3, 0 + 3 * 2],
-  [2 + 3, 1 + 3 * 2],
-  [2 + 3, 2 + 3 * 2],
-  [0 + 3 * 2, 0],
-  [0 + 3 * 2, 1],
-  [0 + 3 * 2, 2],
-  [1 + 3 * 2, 0],
-  [1 + 3 * 2, 1],
-  [1 + 3 * 2, 2],
-  [2 + 3 * 2, 0],
-  [2 + 3 * 2, 1],
-  [2 + 3 * 2, 2],
-  [0 + 3 * 2, 0 + 3],
-  [0 + 3 * 2, 1 + 3],
-  [0 + 3 * 2, 2 + 3],
-  [1 + 3 * 2, 0 + 3],
-  [1 + 3 * 2, 1 + 3],
-  [1 + 3 * 2, 2 + 3],
-  [2 + 3 * 2, 0 + 3],
-  [2 + 3 * 2, 1 + 3],
-  [2 + 3 * 2, 2 + 3],
-  [0 + 3 * 2, 0 + 3 * 2],
-  [0 + 3 * 2, 1 + 3 * 2],
-  [0 + 3 * 2, 2 + 3 * 2],
-  [1 + 3 * 2, 0 + 3 * 2],
-  [1 + 3 * 2, 1 + 3 * 2],
-  [1 + 3 * 2, 2 + 3 * 2],
-  [2 + 3 * 2, 0 + 3 * 2],
-  [2 + 3 * 2, 1 + 3 * 2],
-  [2 + 3 * 2, 2 + 3 * 2],
-].map(e => "" + e)
 
 export const flex2index = ([squre, index]) =>
   index2flex.findIndex(e => e === `${squre},${index}`)
+export const VALUE_4Hang_ITEM_Flat = VALUE_4Hang_ITEM.flat()
+const VALUE_4Lie_ITEM = row2col(VALUE_4Hang_ITEM)
 
-const VALUE4Area = (() => {
-  const t = [] //todo 不优雅 有办法一个函数实现?
-  A81.reduce((all, i) => {
-    const [q, w] = index2flex[i].split(",")
-    const value = VALUE4ROW[q][w]
-    if (i % 9 === 0) {
-      all = []
-      t[index2NumOfHang(i)] = all
-    }
-    all.push(value)
-    return all
-  }, [])
-  return t
-})()
-
-//一个是数组, 一个是函数, 一个传index索引, 一个传arg执行, 所以区别?
-//一个是实时运行, 但相同执行重复执行会浪费时间, 一个是预先全部执行一遍 ,占用空间
-//能外移的外移 避免函数重新执行从而再次执行
-const maybeHeng = VALUE4ROW.map(e => arrayDiff(e))
-const maybeShuu = VALUE4COL.map(e => arrayDiff(e))
-const maybeArea = VALUE4Area.map(e => arrayDiff(e))
-
-const index2RES = []
-export const RES = index2flex.reduce((all, now, index) => {
-  const [x, y] = index2pos[index]
-  const R = maybeHeng[y]
-  const C = maybeShuu[x]
-  const A = maybeArea[index2NumOfArea[index]]
-  const itemValue = index2Value[index]
-  const maybe =
-    itemValue === NEEDRESOLVE
-      ? arrayIntersection(R, C, A).map(e => "m" + e)
-      : []
-  //A91
-
-  if (maybe.length > 1) {
-    var classType = "needSolve"
-  }
-  if (maybe.length === 1) {
-    var classType = "canSolve"
-  }
-  if (maybe.length === 0) {
-    var classType = "hasSolve"
-  }
-
-  const tmp = {
-    value: itemValue,
-    pos: [y, x],
-    maybe,
-    classType,
-  }
-  all[now] = tmp
-  index2RES.push(tmp)
-  return all
-}, {})
-//现在是进页面就默认执行, 改成点击再执行
-//需要深刻理解row,col,1+,0+
-//坐标起始点需要搞清楚
-//两个问题需要搞清楚 0+/1+? col/row?
-
-// const index2OriginMaybeFunc = (i) => [...index2OriginMaybeArr[i]]
-const index2OriginMaybeFunc = i => index2OriginMaybeArr[i]
-const index2OriginMaybeArr = VALUE4ROW.flat()
-  .map(e => "" + e)
-  .map(e => {
-    if (e !== NEEDRESOLVE) return []
-    return [...A91].map(e => "" + e)
-  })
-
-//功能完全相同 思维混乱 不知道什么时候写的  逻辑也看不懂了
 const AreaNum2AreaItems = A90.map(squre =>
   A90.map(index => {
     return flex2index([squre, index])
   })
 )
-const AreaNum2AreaItemsIndex = A81.reduce(
-  (all, i) => {
-    const t = index2NumOfArea[i]
-    all[t].push(i)
-    return all
-  },
-  [...Array(9)].map(e => []) // [...Array(9)].fill([]) 不能 引用相同
-)
-
-const num2Hang9ItemOfMaybe = cacheRunIt(
-  n => num2Hang9ItemOfIndex[n].map(index2OriginMaybeFunc),
-  A90
-)
-
-const num2Liee9ItemOfMaybe = cacheRunIt(
-  n => num2Liee9ItemOfIndex[n].map(index2OriginMaybeFunc),
-  A90
-)
-const num2Area9ItemOfMaybe = cacheRunIt(
-  n => AreaNum2AreaItems[n].map(index2OriginMaybeFunc),
-  A90
-)
-//todo 是否有必要封装这么多层?
-
-//格行排除
-function RSHANGA() {
-  VALUE81.forEach((target, index) => {
-    // if ("" + target === NEEDRESOLVE) return
-    const n = index2NumOfHang(index)
-    // num2Hang9ItemOfIndex[n].map(index2OriginMaybeFunc)
-    num2Hang9ItemOfMaybe[n].forEach(e => deleteMaybe(e, target)) //减去目标
+const VALUE_4Area_ITEM = AreaNum2AreaItems.map(area =>
+  area.map(index => {
+    return VALUE_4Hang_ITEM_Flat[index]
   })
+)
+
+function row2col(row) {
+  return A90.map(x => A90.map(y => row[y][x]))
 }
 
-//格列排除
-function RSLieA() {
-  return VALUE81.map((target, index) => {
-    const n = index2NumOfLiee(index)
-    return num2Liee9ItemOfMaybe[n].map(maybe => {
-      deleteMaybe(maybe, target)
-      return maybe
-    })
-  }).filter((_, i) => i < 9)
-}
-function _RSHANGA() {
-  A81.forEach(index => {
-    const target = VALUE4ROW.flat()[index]
-    if ("" + target === NEEDRESOLVE) return
-    const [_, y] = index2pos[index]
-    //考虑使用es6+ |>符号
-    A90.map(e => [e, y]) //生成目标 这行的pos
-      .map(pos2index) //换成index
-      .map(index2OriginMaybeFunc)
-      //拿到maybe
-      .forEach(e => deleteMaybe(e, target)) //减去目标
-  })
-}
-
-//格宫排除
-function RSGongA() {
-  VALUE81.forEach((target, index) => {
-    const n = index2NumOfArea[index]
-    num2Area9ItemOfMaybe[n].map(e => deleteMaybe(e, target))
-  })
-}
-
-function getJustOne(arr) {}
-//数宫排除https://tieba.baidu.com/p/6327315684?see_lz=1
-function RSGongB() {
-  //静态动态?>
-  return AreaNum2AreaItems.map(area => {
-    const reslut = area
-      .map(i => index2RES[i])
-      .reduce((all, now) => {
-        now.maybe.map(mb => {
-          all[mb] || (all[mb] = [])
-          all[mb].push(now)
-        })
-        return all
-      }, {})
-
-    return Object.keys(reslut).reduce((all, key) => {
-      if (reslut[key].length === 1) all[key] = reslut[key][0].pos
+function string2map() {
+  return (
+    STR &&
+    STR.split("").reduce((all, now, index) => {
+      const n = Math.floor(index / 9)
+      all[n] || (all[n] = [])
+      all[n].push(Number(now))
       return all
-    }, {})
-  })
+    }, [])
+  )
 }
-//数行排除, 基础于格整行, 如果格整行某数字只在一格出现一次,
-//那么把此格数字就是她,可以把其他数字排除了
 
-function RSHangB() {
-  const n = 1
-  num2Hang9ItemOfMaybe[n].ll
-    .reduce(
-      (all, now, index) => {
-        now.map(e => all[e].push(index))
-        return all
-      },
-      [...Array(10)].map(() => [])
-    )
-    .lll("分析完毕, 拿到结果") //可能的数字index -> 可能的格e
-    .map((e, index, maybeHang) => {
-      //maybe就是num2Hang9ItemOfMaybe[n], 但不能用, 引用变了
-      if (e.length === 1) {
-        //如果可能的格只有1格, 那么 index 只在e[0] 格
-        const item = num2Hang9ItemOfMaybe[n][e[0]]
-        item.splice(0, item.length, index + "") //删除所有, 再插入结果
+function cacheRunIt(f, iteration) {
+  const cache = iteration.map(f)
+  return cache
+  return x => cache[x] //?
+}
+
+function check() {
+  let isCorrect = true
+  VALUE_4Hang_ITEM.map(group => {
+    const all = group.map(item => item.value).filter(e => e !== NEEDRESOLVE)
+    if (all.length != [...new Set(all)].length) {
+      isCorrect = false
+    }
+  })
+  VALUE_4Lie_ITEM.map(group => {
+    const all = group.map(item => item.value).filter(e => e !== NEEDRESOLVE)
+    if (all.length != [...new Set(all)].length) {
+      isCorrect = false
+    }
+  })
+  VALUE_4Area_ITEM.map(group => {
+    const all = group.map(item => item.value).filter(e => e !== NEEDRESOLVE)
+    if (all.length != [...new Set(all)].length) {
+      isCorrect = false
+    }
+  })
+  !VALUE_4Hang_ITEM_Flat.map(e => e.value).includes("0") &&
+    console.log("全部完成")
+  console.log("检测结果：", isCorrect)
+}
+export function merge() {
+  VALUE_4Hang_ITEM.map(hang =>
+    hang.map(item => {
+      if (item.maybe.length === 1) {
+        item.value = item.maybe[0]
+        item.maybe = []
       }
     })
+  )
+  check()
+  AllItem()
 }
-function gene(index) {
-  const value = index2Value[index]
-  const classMaybe = index2OriginMaybeArr[index].map(e => "m" + e) //vue class数组必须字符串?,
-
-  // RSLieA() //返回值是什么?
-  // RSHANGA()
-  // RSGongA()
-  // RSHangB()
-
-  if (classMaybe.length > 1) {
-    var classType = "needSolve"
-  }
-  if (classMaybe.length === 1) {
-    var classType = "canSolve"
-  }
-  if (classMaybe.length === 0) {
-    var classType = "hasSolve"
-  }
-
-  return {
-    value,
-    classMaybe,
-    classItem: clickSameValue => [
-      classType,
-      { selected: clickSameValue.x === value },
-    ],
-    classItem: classType,
-  }
+export function HangNum() {
+  VALUE_4Hang_ITEM.map(resolveForNumAdvance)
 }
-//需要考虑 一些函数的缓存是否有必要
 
-//how to let two a90 become a81
-//手动缓存模板 vue不能做吗  有没有这种机制
-export const doubleCache = A90.map(squre =>
-  A90.map(index => flex2index([squre, index])).map(gene)
-)
-export const doubleCache1 = A90.map(squre =>
-  A90.map(index => gene(flex2index([squre, index])))
-)
+export function LieNum() {
+  VALUE_4Lie_ITEM.map(resolveForNumAdvance)
+}
+
+export function AreaNum() {
+  VALUE_4Area_ITEM.map(resolveForNumAdvance)
+}
+export function AllNum() {
+  VALUE_4Hang_ITEM.map(resolveForNumAdvance)
+  VALUE_4Lie_ITEM.map(resolveForNumAdvance)
+  VALUE_4Area_ITEM.map(resolveForNumAdvance)
+}
+
+export function HangItem() {
+  VALUE_4Hang_ITEM.map(resolveForItemBase)
+}
+export function LieItem() {
+  VALUE_4Lie_ITEM.map(resolveForItemBase)
+}
+export function AreaItem() {
+  VALUE_4Area_ITEM.map(resolveForItemBase)
+}
+export function AllItem() {
+  VALUE_4Hang_ITEM.map(resolveForItemBase)
+  VALUE_4Lie_ITEM.map(resolveForItemBase)
+  VALUE_4Area_ITEM.map(resolveForItemBase)
+}
+
+AllItem()
+resolveForNumAdvance(VALUE_4Area_ITEM[3]).ll
+function resolveForNumAdvance(group) {
+  return group
+    .map(item => item.maybe)
+    .reduce((all, now, index) => {
+      now.map(value => {
+        all.push({
+          value,
+          index,
+        })
+      })
+      return all
+    }, [])
+    .filter(({ value }, _, arr) => {
+      return arr.filter(e => e.value === value).length === 1
+    })
+}
+
+function resolveForItemBase(hang) {
+  const hangValue = hang.map(e => e.value).filter(e => e !== NEEDRESOLVE)
+  const hangMaybe = hang.map(e => e.maybe).filter(e => e.length > 1)
+  //副作用
+  hangValue.map(value => deleteGroupMaybeOfAValue(hangMaybe, value))
+}
+function deleteGroupMaybeOfAValue(arr, value) {
+  arr.map(item => deleteMaybe(item, value)) //value是0的话不是已知值不需要排除
+}
+function deleteMaybe(arr, target) {
+  const index = arr.indexOf(target + "")
+  if (index === -1) return //没有找到 必须返回, -1给splice会有其他效果
+  arr.splice(index, 1) //是否需要可变数组 git管理以防无法复原的bug
+  return arr
+  //25个Ok 56个todoing
+}
--- src/sdk.vue
+++ src/sdk.vue
@@ -1,229 +1,287 @@
 <template>
-  <div>
-    <div class="main">
-      <div class="S" v-for="S in A90" :key="S">
-        <div
-          class="item"
-          @mouseover="itemMouse"
-          @click="itemClick(S,I)"
-          v-for="I in A90"
-          :class="[ XX[S][I].classItem, hoverValue.x === XX[S][I].value && 'selected' ]"
-          :key="I"
-        >
-          <span v-if="XX[S][I].value !== NEEDRESOLVE">{{XX[S][I].value}}</span>
+  <div class="main">
+    <div class="suduko">
+      <div class="squre" v-for="S in [0,1,2,3,4,5,6,7,8]" :key="S">
+        <div v-for="I in [0,1,2,3,4,5,6,7,8]" :key="I">
+          <!-- just for local SUDUKO -->
+          <!-- <div style="display:none">{{SUDUKO = arrSUDUKO[S][I]}}</div> -->
+          <!-- <div style="display:none">{{funcSUDUKO([S,I])}}</div> -->
+          <!-- <div style="display:none">{{SUDUKO = gene(arrSUDUKO[S][I])}}</div> -->
+          <div style="display:none">{{SUDUKO = gene([S,I])}}</div>
+          <!-- <div style="display:none">{{ (()=>1)().ll }}</div>  -->
+          <!-- <div style="display:none">{{ 33..ll }}</div> -->
           <div
-            class="maybe"
-            v-else
-            v-for="maybe in A91"
-            :class="[ XX[S][I].classMaybe,  hoverValue.x === ''+maybe && 'selected']"
-            :key="maybe"
-          >{{maybe}}</div>
+            :class="['item', SUDUKO.clsItem, hoverValue.x === SUDUKO.value && 'selected' ]"
+            @mouseover="e=>itemMouse(e,hoverValue)"
+            @click="itemClick(S,I)"
+          >
+            <span v-if="SUDUKO.value !== NEEDRESOLVE">{{SUDUKO.value}}</span>
+            <div
+              v-else
+              :class="['maybe', SUDUKO.clsMaybe,  hoverValue.x === maybe && 'selected']"
+              v-for="maybe in ['1','2','3','4','5','6','7','8','9']"
+              :key="maybe"
+            >{{maybe}}</div>
+          </div>
         </div>
       </div>
     </div>
-
-    <div class="main">
-      <div class="S" v-for="S in A90" :key="S">
-        <div class="item" v-for="I in A90" :key="I">
-          <span>{{S}}{{I}}</span>
-        </div>
-      </div>
+    <div class="control">
+      <button @click="flash(HangItem)">HangItem</button>
+      <button @click="flash(LieItem)">LieItem</button>
+      <button @click="flash(AreaItem)">AreaItem</button>
+      <button @click="flash(AllItem)">all</button>
+      <br />
+      <button @click="flash(HangNum)">HangNum</button>
+      <button @click="flash(LieNum)">Liee</button>
+      <button @click="flash(AreaNum)">AreaNum</button>
+      <button @click="flash(AllNum)">all</button>
+      <br />
+      <button @click="flash(merge)">merge</button>
+      <span>{{flashN}}</span>
     </div>
   </div>
+  <!-- visible改成v-show 不占用dom节点? -->
 </template>
 
-  <!-- {{eval('debugger') /*a way for debug*/}} -->
-        <!--         
-        :classs="double3(S, I, hoverValue).classItem"
-        :classss="double(S, I, ).classItem(hoverValue)"
-        :classx="doubles(S, I, ).classItem"-->
-
-        <!-- <no style="display:none">{{dbg = double(S, I,)}}</no> -->
-       
 <style lang="scss" >
 .main {
-  display: flex;
-  flex-wrap: wrap;
-  width: 420px;
-  .S {
-    width: 120px;
-    margin: 5px;
+  .suduko {
     display: flex;
     flex-wrap: wrap;
-    // &:hover {
-    //   .item {
-    //     background: mix(#aae, #fff, 50%);
-    //     &.canSolve {
-    //       background: mix(#3ee, #fff, 50%);
-    //     }
-    //     &.needSolve {
-    //       background: mix(#aee, #fff, 50%);
-    //     }
-    //   }
-    // }
-    .item {
-      width: 40px;
-      height: 40px;
+    width: 420px;
+    .squre {
+      width: 120px;
+      margin: 5px;
       display: flex;
       flex-wrap: wrap;
-      justify-content: center;
-      align-items: center;
-      background: #eee;
-      // transition: all 0.1s;
-      &:hover,
-      &.canSolve:hover,
-      &.needSolve:hover {
-        background: #aea;
-      }
-      &.canSolve {
-        background: #3ee;
-      }
-      &.needSolve {
-        background: #aee;
-      }
-      &.selected {
-        background: #aea;
-      }
-      .maybe {
-        font-size: 11px;
-        width: 33%;
-        height: 33%;
-        text-align: center;
-        visibility: hidden;
+      // &:hover {
+      //   .item {
+      //     background: mix(#aae, #fff, 50%);
+      //     &.canSolve {
+      //       background: mix(#3ee, #fff, 50%);
+      //     }
+      //     &.needSolve {
+      //       background: mix(#aee, #fff, 50%);
+      //     }
+      //   }
+      // }
+      .item {
+        font-weight: 600;
+        width: 40px;
+        height: 40px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        background: #eee;
+        // transition: all 0.1s;
+        &:hover,
+        &.canSolve:hover,
+        &.needSolve:hover {
+          background: #aea;
+        }
+        &.canSolve {
+          background: #0ea;
+        }
+        &.needSolve {
+          background: #aee;
+        }
         &.selected {
           background: #aea;
         }
-        &.m1 {
-          &:nth-of-type(1) {
-            visibility: inherit;
+        .maybe {
+          font-weight: 100;
+          font-size: 11px;
+          width: 33%;
+          height: 33%;
+          text-align: center;
+          visibility: hidden;
+          &.selected {
+            background: #aea;
           }
-        }
-        &.m2 {
-          &:nth-of-type(2) {
-            visibility: inherit;
+          &.m1 {
+            div &:nth-of-type(1) {
+              visibility: inherit;
+            }
           }
-        }
-        &.m3 {
-          &:nth-of-type(3) {
-            visibility: inherit;
+          &.m2 {
+            div &:nth-of-type(2) {
+              visibility: inherit;
+            }
           }
-        }
-        &.m4 {
-          &:nth-of-type(4) {
-            visibility: inherit;
+          &.m3 {
+            div &:nth-of-type(3) {
+              visibility: inherit;
+            }
           }
-        }
-        &.m5 {
-          &:nth-of-type(5) {
-            visibility: inherit;
+          &.m4 {
+            div &:nth-of-type(4) {
+              visibility: inherit;
+            }
           }
-        }
-        &.m6 {
-          &:nth-of-type(6) {
-            visibility: inherit;
+          &.m5 {
+            div &:nth-of-type(5) {
+              visibility: inherit;
+            }
           }
-        }
-        &.m7 {
-          &:nth-of-type(7) {
-            visibility: inherit;
+          &.m6 {
+            div &:nth-of-type(6) {
+              visibility: inherit;
+            }
           }
-        }
-        &.m8 {
-          &:nth-of-type(8) {
-            visibility: inherit;
+          &.m7 {
+            div &:nth-of-type(7) {
+              visibility: inherit;
+            }
           }
-        }
-        &.m9 {
-          &:nth-of-type(9) {
-            visibility: inherit;
+          &.m8 {
+            div &:nth-of-type(8) {
+              visibility: inherit;
+            }
+          }
+          &.m9 {
+            div &:nth-of-type(9) {
+              visibility: inherit;
+            }
           }
         }
       }
+    }
+  }
+  .control {
+    button {
+      margin: 5px;
     }
   }
 }
 </style>
 <script>
-import { reactive, ref } from "vue";
+import { reactive, ref, watchEffect, watch } from "vue";
 import {
   NEEDRESOLVE,
-  doubleCache as XX,
-  RES,
-  A90,
   A91,
-  pos2index,
   flex2index,
   index2flex,
-  index2pos
+  index2pos,
+  HangItem,
+  LieItem,
+  AreaItem,
+  HangNum,
+  LieNum,
+  AreaNum,
+  AllItem,
+  AllNum,
+  VALUE_4Hang_ITEM_Flat,
+  merge
 } from "./sdk.js";
-function superSuspend(n) {
-  const q = Date.now();
-  while (Date.now() - q < n) {}
-}
+window.q = [];
 
 export default {
   setup() {
     let hoverValue = reactive({ x: "" });
-    let dbg = ref(2222);
-    let tmp = reactive(2);
-    const r = ref(2);
+    let SUDUKO = ref();
+    let x = ref(1);
+    const flashN = ref(0);
+    function flash(func) {
+      func();
+      flashN.value++;
+    }
+    // watchEffect(() => {
+    //   // flashN.value;
+    //   SUDUKO.value;
+    //   "watchEffect".ll;
+    //   window.q.push(SUDUKO);
+    // });
+    // watch(SUDUKO, () => {
+    //   // flashN.value;
+    //   "watch".ll;
+    // });
+
+    function gene(flex) {
+      const index = flex2index(flex);
+      const data = VALUE_4Hang_ITEM_Flat[index];
+      if (data.maybe.length > 1) {
+        var clsItem = "needSolve";
+      }
+      if (data.maybe.length === 1) {
+        var clsItem = "canSolve";
+      }
+      if (data.maybe.length === 0) {
+        var clsItem = "hasSolve";
+      }
+      var clsMaybe = data.maybe.map(e => "m" + e);
+      return {
+        ...data,
+        clsItem,
+        clsMaybe
+      };
+    }
     return {
-      XX,
-      A90,
       A91,
       NEEDRESOLVE,
       itemMouse,
+      itemClick,
       hoverValue,
-      tmp,
-      r,
-      dbg,
-      eval,
-      log: console.log,
-      itemClick
+      SUDUKO,
+      HangItem,
+      LieItem,
+      AreaItem,
+      HangNum,
+      LieNum,
+      AreaNum,
+      AllItem,
+      AllNum,
+      merge,
+      flash,
+      flashN,
+      gene
     };
-
-    function itemMouse(e) {
-      let x;
-      if (e.currentTarget.classList.contains("hasSolve")) {
-        x = e.currentTarget.innerText;
-      } else {
-        x = "";
-      }
-      // r.value = tmp = hoverValue.x = x;
-    }
-    function itemClick(S, I) {
-      const flex = [S, I].ll;
-      const index = flex2index(flex).ll;
-      index2pos[index].ll;
-    }
   }
 };
-
-//计算函数执行次数 寻找可以空间换时间函数  比较两次策略性能
-//手动打孔 增加难度?
-const tttt = [
-  [2, 0, 0, 0, 0, 0, 0, 0, 0],
-  [7, 0, 0, 0, 0, 0, 0, 0, 0],
-  [5, 0, 0, 0, 0, 0, 0, 0, 0],
-  [6, 0, 0, 0, 0, 0, 0, 0, 0],
-  [4, 0, 0, 0, 0, 0, 0, 0, 0],
-  [9, 0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [3, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
+function itemMouse(e, hoverValue) {
+  if (e.currentTarget.classList.contains("hasSolve")) {
+    // hoverValue.x = e.currentTarget.innerText;
+  } else {
+    hoverValue.x = "";
+  }
+}
+function itemClick(S, I) {
+  const flex = [S, I].ll;
+  const index = flex2index(flex).ll;
+  index2pos[index].ll;
+}
 // if (0) asas//不报错??
 
 //全部以index为中介
-
-//缓存 模板执行太多了
 // 行格排除  行数排除
 // 数排除  数确定只能在某个格
 // 格排除  这个格只能填这个数
 
-//可以缓存的根本原因是参数有限固定, 预先执行
-//考虑底层不缓存  最上层抽象
+//缓存 模板执行太多了
+
 //考虑使用es6+ |>符号
+//todo 是否有必要封装这么多层?
+
+//todo 引号问题
+
+//自动循环执行排除  直到执行结果没有新排除单元
+
+//可以缓存的根本原因是参数有限固定, 预先执行
+//需要考虑 一些函数的缓存是否有必要
+//手动缓存模板 vue不能做吗  有没有这种机制
+//考虑底层不缓存  最上层抽象
+//现在的策略是把所有函数缓存一边,感觉没必要, 1根据性能,2顶层缓存,下层不变无感
+
+//计算函数执行次数 寻找可以空间换时间函数  比较两次策略性能
+//todo value 和maybevalue是一个值  互斥
+
+//一个是数组, 一个是函数, 一个传index索引, 一个传arg执行, 所以区别?
+//一个是实时运行, 但相同执行重复执行会浪费时间, 一个是预先全部执行一遍 ,占用空间
+//能外移的外移 避免函数重新执行从而再次执行
+
+//坐标起始点需要搞清楚
+//需要深刻理解row,col,1+,0+
+//3个问题需要搞清楚 0+/1+? col/row? ''?
 </script>
 
